dba393001b9d959c935cfa1e13c2afa6197efab0
LanoCodes <del.pow1@gmail.com>
2022-11-19 07:48:11 -0500

    some more code cleanup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 with open("./Input/Names/some_ceos.txt", mode="r") as invitees_file:
     invitees = invitees_file.readlines()
 
-with  open("./Input/Letters/template.txt", mode="r") as starting_letter:
+with open("./Input/Letters/template.txt", mode="r") as starting_letter:
     template = starting_letter.readlines()
     new_letter_addressee = []
 
@@ -15,13 +15,12 @@ file_name_start = "letter_for_"
 file_names = []
 for name in invitees:
     file_names.append(file_name_start + name.strip() + ".txt")
-print(file_names)
 
 letter = template.copy()
 letters_to_send = []
 for index, addressee in enumerate(new_letter_addressee):
     letter[0] = new_letter_addressee[index]
     letters_to_send.append(letter)
-    print(letters_to_send[index])
+
     with open(os.path.join("/Users/delanopowell/Desktop/100 DoC/Projects/Mail Merge /Output/ReadyToSend", file_names[index]), mode="w") as files:
         files.write(''.join(letters_to_send[index]))
